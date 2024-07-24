7e1a9be83701fe45a771e269b0cdc144114fb977
이찬진 <chanjin23@naver.com>
2024-07-24 16:50:10 +0900

    괄호의 값 소요시간:40min 메모리:17740KB 시간: 212ms

---
+++ src/baekjoon/codingtest_loadmap/part2weaknesscheck/Sol2504.java
@@ -0,0 +1,69 @@
+package baekjoon.codingtest_loadmap.part2weaknesscheck;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Sol2504 {
+    //괄호의 값
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String n = sc.nextLine();
+
+        String[] strs = n.split("");
+        int total=0;
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < strs.length; ++i) {
+            String str = strs[i];
+            int totalTmp = 0;
+            try {
+                if (str.equals("(") || str.equals("[")) {
+                    stack.push(str);
+                } else if (str.equals(")")) {
+                    while (true) {
+                        String tmp = stack.pop();
+                        if (tmp.equals("(")) {
+                            if (totalTmp == 0) stack.push("2");
+                            else {
+                                int i1 = 2 * totalTmp;
+                                stack.push(String.valueOf(i1));
+                            }
+                            break;
+                        } else {
+                            int num = Integer.parseInt(tmp);
+                            totalTmp += num;
+                        }
+                    }
+                } else {
+                    while (true) {
+                        String tmp = stack.pop();
+                        if (tmp.equals("[")) {
+                            if (totalTmp == 0) stack.push("3");
+                            else {
+                                int i1 = 3 * totalTmp;
+                                stack.push(String.valueOf(i1));
+                            }
+                            break;
+                        } else {
+                            int num = Integer.parseInt(tmp);
+                            totalTmp += num;
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(0);
+                return;
+            }
+        }   //for
+
+        try {
+            for (String s : stack) {
+                total += Integer.parseInt(s);
+            }
+        } catch (Exception e) {
+            System.out.println(0);
+            return;
+        }
+        System.out.println(total);
+    }
+}
