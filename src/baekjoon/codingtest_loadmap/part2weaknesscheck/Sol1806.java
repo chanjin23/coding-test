0bb38baa868d47bf574cf873eea16aa6b1798a0d
이찬진 <chanjin23@naver.com>
2025-01-19 14:44:12 +0900

    백준1806 부분합 : 투포인터 이용, 걸린시간: 30min

--- src/baekjoon/codingtest_loadmap/part2weaknesscheck/Sol1806.java
+++ src/baekjoon/codingtest_loadmap/part2weaknesscheck/Sol1806.java
@@ -1,39 +1,52 @@
 package baekjoon.codingtest_loadmap.part2weaknesscheck;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Sol1806 {
     //부분 합
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
+        long n = Long.parseLong(st.nextToken());
+        long s = Long.parseLong(st.nextToken());
+        long[] arr = new long[(int) n];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; ++i) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Long.parseLong(st.nextToken());
         }
 
-        int left = 0;
-        int right = 0;
-        int sum = 0;
-        int minLen = Integer.MAX_VALUE;
-        for (; right < n; ++right) {
-            sum+=arr[right];
+        int lt = 0;
+        int rt = 0;
+        long tmp = 0;
+        int answer = Integer.MAX_VALUE;
 
-            while (sum >= s) {
-                minLen = Math.min(minLen, right - left + 1);
-                sum -= arr[left++];
+        while (rt < n) {
+            if (tmp < s) {
+                tmp += arr[rt];
+                rt++;
+            } else {
+                answer = Math.min(answer, rt - lt);
+                tmp -= arr[lt];
+                lt++;
             }
         }
-        if (minLen == Integer.MAX_VALUE) System.out.println(0);
-        else System.out.println(minLen);
+
+        while (tmp >= s) {
+            answer = Math.min(answer, rt - lt);
+            tmp -= arr[lt];
+            lt++;
+        }
+
+
+        if (answer == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(answer);
+        }
     }
 
 }
