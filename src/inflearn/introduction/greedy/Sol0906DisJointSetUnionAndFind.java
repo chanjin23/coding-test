2f3fcb94f9a1ef30fc696b692a389131bb348794
이찬진 <chanjin23@naver.com>
2025-01-21 15:50:05 +0900

    인프런0906 친구인가? : union & find 이용, 걸린시간 20m

--- src/inflearn/introduction/greedy/Sol0906DisJointSetUnionAndFind.java
+++ src/inflearn/introduction/greedy/Sol0906DisJointSetUnionAndFind.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.io.*;
 
 public class Sol0906DisJointSetUnionAndFind {
+    static int[] arr;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,58 +12,34 @@ public class Sol0906DisJointSetUnionAndFind {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n + 1];
-        ArrayList<Point> list = new ArrayList<>();
+        arr = new int[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            arr[i] = i;
+        }
+
         for (int i = 0; i < m; ++i) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            list.add(new Point(x, y));
+            union(x, y);
         }
 
         st = new StringTokenizer(br.readLine());
         int friend1 = Integer.parseInt(st.nextToken());
         int friend2 = Integer.parseInt(st.nextToken());
 
-        arr[friend1] = 1;
-        arr[friend2] = 2;
-
-        for (int i=0; i < m; ++i) {
-            boolean flag = false;
-            for (int j = 0; j < m; ++j) {
-                int student1 = list.get(j).x;
-                int student2 = list.get(j).y;
-                if (arr[student1] != 0 && arr[student2] != 0) {
-//                    System.out.println("arr[student1] = " + arr[student1]);
-//                    System.out.println("arr[student2] = " + arr[student2]);
-                    if (arr[student1] != arr[student2]) {
-                        //만난것
-                        System.out.println("YES");
-                        return;
-                    }
-                } else if (arr[student1] != 0) {
-                    arr[student2] = arr[student1];
-                } else if (arr[student2] != 0) {
-                    arr[student1] = arr[student2];
-                } else {
-                    //둘다 0인경우
-                    flag = true;
-                }
-//                System.out.println(Arrays.toString(arr));
-            }
-            if(!flag) break;
-        }
-
-        System.out.println("NO");
+        if (find(friend1)==find(friend2)) System.out.println("YES");
+        else System.out.println("NO");
     }
 
-    public static class Point {
-        int x;
-        int y;
+    public static void union(int x, int y) {
+        int a = find(x);
+        int b = find(y);
+        if(arr[a]!=arr[b]) arr[a] = arr[b];
+    }
 
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+    public static int find(int value) {
+        if(arr[value]==value) return value;
+        else return arr[value] = find(arr[value]);
     }
 }
