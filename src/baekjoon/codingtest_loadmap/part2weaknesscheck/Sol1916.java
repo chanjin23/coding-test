853ed404b5c1a0e96c440e8f4ea0460f5da04b4a
이찬진 <chanjin23@naver.com>
2025-01-21 14:02:32 +0900

    백준1916 최소 비용 구하기 : 다익스트라 알고리즘 이용, 걸린시간 1h 다시풀기⭐⭐⭐

--- src/baekjoon/codingtest_loadmap/part2weaknesscheck/Sol1916.java
+++ src/baekjoon/codingtest_loadmap/part2weaknesscheck/Sol1916.java
@@ -1,52 +1,56 @@
 package baekjoon.codingtest_loadmap.part2weaknesscheck;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Sol1916 {
-    static ArrayList<ArrayList<Point>> list;
+    static int n, m, start, arrive;
     static int[] arr;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        list = new ArrayList<>();
+    static PriorityQueue<Point> queue = new PriorityQueue<>();
+    static ArrayList<ArrayList<Point>> list = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
         arr = new int[n + 1];
         Arrays.fill(arr, Integer.MAX_VALUE);
-        for (int i = 0; i <= n; ++i) {
-            list.add(new ArrayList<>());
-        }
+
+        for (int i = 0; i <= n; ++i) list.add(new ArrayList<>());
+
         for (int i = 0; i < m; ++i) {
-            int currentNode = sc.nextInt();
-            int nextNode = sc.nextInt();
-            int weight = sc.nextInt();
-            list.get(currentNode).add(new Point(nextNode, weight));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int first = Integer.parseInt(st.nextToken());
+            int last = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            list.get(first).add(new Point(last, weight));
         }
-        solution(1);
-        for (int i = 2; i <= n; ++i) {
-            if (arr[i] == Integer.MAX_VALUE) {
-                System.out.println(i + " : impossible");
-            } else {
-                System.out.println(i + " : " + arr[i]);
-            }
-        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        start = Integer.parseInt(st.nextToken());
+        arrive = Integer.parseInt(st.nextToken());
+
+        solution(start);
+
+        System.out.println(arr[arrive]);
+
     }
 
-    public static void solution(int nextNode) {
-        PriorityQueue<Point> pQ = new PriorityQueue<>();
-        pQ.offer(new Point(nextNode, 0));
-        while (!pQ.isEmpty()) {
-            Point tmp = pQ.poll();
+    public static void solution(int start) {
+        arr[start] = 0;
+        queue.offer(new Point(start, 0));
+        while (!queue.isEmpty()) {
+            Point tmp = queue.poll();
             int currentNode = tmp.nextNode;
             int currentWeight = tmp.weight;
 
-            if (arr[currentNode] < currentWeight) continue;//arr[현재노드] 값보다 가중치값이 크면 갱신할필요가 x, 없으면 시간복잡도 상승
+            if (arr[currentNode] < currentWeight) continue;
             for (Point p : list.get(currentNode)) {
                 if (arr[p.nextNode] > currentWeight + p.weight) {
                     arr[p.nextNode] = currentWeight + p.weight;
-                    pQ.offer(new Point(p.nextNode, currentWeight + p.weight));
+                    queue.offer(new Point(p.nextNode, currentWeight + p.weight));
                 }
             }
         }
@@ -56,14 +60,13 @@ public class Sol1916 {
         int nextNode;
         int weight;
 
-        public Point(int nextNode, int weight) {
+        Point(int nextNode, int weight) {
             this.nextNode = nextNode;
             this.weight = weight;
         }
 
-        @Override
         public int compareTo(Point o) {
-            return weight - o.weight;
+            return this.weight - o.weight;
         }
     }
 }
