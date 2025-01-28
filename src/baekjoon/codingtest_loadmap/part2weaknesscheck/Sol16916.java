2f8150f7f03058b8b5911aeced752ecd5ecb3b0d
이찬진 <chanjin23@naver.com>
2025-01-28 15:16:13 +0900

    백준16916 부분 문자열 : 라빈카프 이용, 시간초과

--- src/baekjoon/codingtest_loadmap/part2weaknesscheck/Sol16916.java
+++ src/baekjoon/codingtest_loadmap/part2weaknesscheck/Sol16916.java
@@ -6,32 +6,38 @@ public class Sol16916 {
     //부분 문자열
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String p = sc.nextLine();
         String s = sc.nextLine();
+        String p = sc.nextLine();
 
-        char[] ch = p.toCharArray();
-        for (int i=0;i<ch.length;++i) {
-            if(ch[i]!=s.charAt(0)) continue;
+        char[] chs = s.toCharArray();
+        char[] chp = p.toCharArray();
 
-            boolean check=false;
-            try {
-                for (int j = i; j < i + s.length(); ++j) {
-                    if (ch[j] != s.charAt(j - i)) {
-                        check = true;
+        int hashS = 0;
+        int hashP = 0;
+
+        for (int i = 0; i < chp.length; ++i) {
+            hashS += chs[i];
+            hashP += chp[i];
+        }
+
+        for (int i = 0; i < chs.length - chp.length + 1; ++i) {
+            if (hashS == hashP) {
+                boolean flag = false;
+                for (int j = i; j < i + chp.length; ++j) {
+                    if (chs[j] != chp[j - i]) {
+                        flag = true;
                         break;
                     }
                 }
-            } catch (Exception e) {
-                System.out.println(0);
-                return;
-            }
-
-            if(!check){
-                System.out.println(1);
-                return;
+                if(!flag){
+                    System.out.println(1);
+                    return;
+                }
+            } else {
+                if(i==chs.length-chp.length) continue;
+                hashS = hashS - chs[i] + chs[i + chp.length];
             }
         }
-
         System.out.println(0);
     }
 }
