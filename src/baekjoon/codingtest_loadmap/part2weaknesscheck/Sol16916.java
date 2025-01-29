99f6b589a042b37b4b76c739a2779aec4b5b585c
이찬진 <chanjin23@naver.com>
2025-01-29 19:20:49 +0900

    백준16916 부분 문자열 : KMP이용, 다시풀기 ⭐⭐⭐

--- src/baekjoon/codingtest_loadmap/part2weaknesscheck/Sol16916.java
+++ src/baekjoon/codingtest_loadmap/part2weaknesscheck/Sol16916.java
@@ -1,5 +1,6 @@
 package baekjoon.codingtest_loadmap.part2weaknesscheck;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sol16916 {
@@ -9,35 +10,46 @@ public class Sol16916 {
         String s = sc.nextLine();
         String p = sc.nextLine();
 
-        char[] chs = s.toCharArray();
-        char[] chp = p.toCharArray();
+        System.out.println(KMP(s,p));
+    }
 
-        int hashS = 0;
-        int hashP = 0;
+    public static int KMP(String s, String p) {
+        int[] arr = makeTable(p);
+        int sLen = s.length();
+        int pLen = p.length();
+        int idx = 0;
 
-        for (int i = 0; i < chp.length; ++i) {
-            hashS += chs[i];
-            hashP += chp[i];
-        }
+        for (int i = 0; i < sLen; ++i) {
+            while (idx > 0 && s.charAt(i) != p.charAt(idx)) {
+                idx = arr[idx - 1];
+            }
 
-        for (int i = 0; i < chs.length - chp.length + 1; ++i) {
-            if (hashS == hashP) {
-                boolean flag = false;
-                for (int j = i; j < i + chp.length; ++j) {
-                    if (chs[j] != chp[j - i]) {
-                        flag = true;
-                        break;
-                    }
+            if (s.charAt(i) == p.charAt(idx)) {
+                if (idx == pLen - 1) {
+                    return 1;
+                } else {
+                    idx += 1;
                 }
-                if(!flag){
-                    System.out.println(1);
-                    return;
-                }
-            } else {
-                if(i==chs.length-chp.length) continue;
-                hashS = hashS - chs[i] + chs[i + chp.length];
             }
         }
-        System.out.println(0);
+        return 0;
+    }
+
+    public static int[] makeTable(String p) {
+        int[] arr = new int[p.length()];
+        int idx = 0;
+
+        for (int i = 1; i < arr.length; ++i) {
+            //연속된 패턴에서 일치하지 않으면 idx 한칸아래의 배열에 접근해서 idx 변경
+            while (idx > 0 && p.charAt(idx) != p.charAt(i)) {
+                idx = arr[idx - 1];
+            }
+
+            if (p.charAt(idx) == p.charAt(i)) {
+                idx += 1;
+                arr[i] = idx;
+            }
+        }
+        return arr;
     }
 }
