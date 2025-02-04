813a175aaee6223615d81c8820ad206243d2776f
이찬진 <chanjin23@naver.com>
2025-02-04 21:40:10 +0900

    백준1062 가르침 : dfs 이용, 시간복잡도 계산하기

--- src/Main.java
+++ src/Main.java
@@ -7,32 +7,61 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Long> list;
+    static String[] input;
+    static boolean[] alphabet;
+    static int k, answer = 0, n;
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
-        int n = scan.nextInt();
-        list = new ArrayList<>();
-
-        if(n <= 10) System.out.println(n);
-        else if(n > 1022) System.out.println("-1");
-        else {
-            for(int i = 0; i < 10; i++) {
-                bp(i, 1);
-            }
-            Collections.sort(list);
-
-            System.out.println(list.get(n));
+        input = new String[n];
+        alphabet = new boolean[26];
+        alphabet['a' - 'a'] = true;
+        alphabet['c' - 'a'] = true;
+        alphabet['i' - 'a'] = true;
+        alphabet['n' - 'a'] = true;
+        alphabet['t' - 'a'] = true;
+        for (int i = 0; i < n; ++i) {
+            input[i] = br.readLine();
         }
+
+        if (k < 5) {
+            System.out.println(0);
+            return;
+        } else if (k == 26) {
+            System.out.println(n);
+            return;
+        }
+
+        dfs(0,0);
+        System.out.println(answer);
     }
 
-    public static void bp(long num, int idx) {
-        if(idx > 10) return;
-
-        list.add(num);
-        for(int i = 0; i < num % 10; i++) {
-            bp((num * 10) + i, idx + 1);
+    public static void dfs(int alpha, int level) {
+        if (level == k - 5) {
+            int total = 0;
+            for (int i = 0; i < n; ++i) {
+                boolean flag = false;
+                for (char c : input[i].toCharArray()) {
+                    if (!alphabet[c - 'a']) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag) total++;
+            }
+            answer = Math.max(total, answer);
+        } else {
+            for (int i = alpha; i < 26; ++i) {
+                if (!alphabet[i]) {
+                    alphabet[i] = true;
+                    dfs(i,level + 1);
+                    alphabet[i] = false;
+                }
+            }
         }
     }
 }
