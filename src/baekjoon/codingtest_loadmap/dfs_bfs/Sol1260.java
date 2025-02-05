1424d89fd6a87e59c6394ef56d697758c2e02606
이찬진 <chanjin23@naver.com>
2025-02-05 18:07:25 +0900

    백준1260 dfs와 bfs : dfs, bfs 인접리스트이용, 걸린시간 : 24min

--- src/baekjoon/codingtest_loadmap/dfs_bfs/Sol1260.java
+++ src/baekjoon/codingtest_loadmap/dfs_bfs/Sol1260.java
@@ -1,97 +1,83 @@
 package baekjoon.codingtest_loadmap.dfs_bfs;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Sol1260 {
     //DFS와 BFS
     static int n, m, v;
-    static Point[] arr,arr2;
-    static boolean check[];
+    static List<List<Integer>> list;
+    static int[] arr;
+    static List<Integer> dfsList, bfsList;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         v = Integer.parseInt(st.nextToken());
 
-        arr = new Point[n + 1];
-        arr2 = new Point[n + 1];
-        check = new boolean[n + 1];
-        for (int i = 1; i < n + 1; ++i) {
-            arr[i] = new Point(new PriorityQueue<>());
-            arr2[i] = new Point(new PriorityQueue<>());
+        list = new ArrayList<>();
+        dfsList = new ArrayList<>();
+        bfsList = new ArrayList<>();
+        arr = new int[n + 1];
+
+        for (int i = 0; i <= n; ++i) {
+            list.add(new ArrayList<>());
         }
 
         for (int i = 0; i < m; ++i) {
             st = new StringTokenizer(br.readLine());
-            int first = Integer.parseInt(st.nextToken());
-            int second = Integer.parseInt(st.nextToken());
-            arr[first].list.add(second);
-            arr2[first].list.add(second);
-            arr[second].list.add(first);
-            arr2[second].list.add(first);
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            list.get(node1).add(node2);
+            list.get(node2).add(node1);
         }
 
-//        System.out.println(arr[1].list.poll());
-//        System.out.println(arr[1].list.poll());
+        for (int i = 1; i <= n; ++i) Collections.sort(list.get(i));
 
-//        for (int i = 1; i < n + 1; ++i) {
-//            PriorityQueue<Integer> queue = arr[i].list;
-//            int size = queue.size();
-//            for (int j = 0; j < size; ++j) {
-//                System.out.print(queue.poll()+" ");
-//            }
-//            System.out.println();
-//        }
-//        System.out.println("----------------------");
-
-        check[v] = true;
+        arr[v] = 1;
+        dfsList.add(v);
         dfs(v);
+
+        Arrays.fill(arr, 0);
+        bfs(v);
+
+        for (int i = 0; i < dfsList.size(); ++i) {
+            System.out.print(dfsList.get(i)+" ");
+        }
         System.out.println();
 
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] bfsCheck = new boolean[n + 1];
+        for (int i = 0; i < bfsList.size(); ++i) {
+            System.out.print(bfsList.get(i)+" ");
+        }
+    }
 
-        q.add(v);
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; ++i) {
-                Integer tmp = q.poll();
-                if(!bfsCheck[tmp]) System.out.print(tmp + " ");
-                bfsCheck[tmp] = true;
-
-                int len = arr2[tmp].list.size();
-                for (int j = 0; j < len; ++j) {
-                    Integer poll = arr2[tmp].list.poll();
-                    if (!bfsCheck[poll]) q.offer(poll);
-                }
+    public static void dfs(int node) {
+        for (int i : list.get(node)) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+                dfsList.add(i);
+                dfs(i);
             }
         }
-
     }
 
-    private static void dfs(int node) {
-        Integer nextNode;
-        System.out.print(node + " ");
-        while (true) {
-            nextNode = arr[node].list.poll();
-            if (nextNode == null) return;
-            if (!check[nextNode]) break;
-        }
-        check[nextNode] = true;
-        dfs(nextNode);
-    }
-
-    public static class Point {
-        PriorityQueue<Integer> list;
-
-        public Point(PriorityQueue<Integer> list) {
-            this.list = list;
+    public static void bfs(int node) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
+        arr[node] = 1;
+        bfsList.add(node);
+        while (!q.isEmpty()) {
+            int tmp = q.poll();
+            for (int i : list.get(tmp)) {
+                if (arr[i] == 0) {
+                    arr[i] = 1;
+                    bfsList.add(i);
+                    q.offer(i);
+                }
+            }
         }
     }
 }
