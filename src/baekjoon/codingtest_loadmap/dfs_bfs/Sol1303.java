2d0bba20fcaf0cddcc5b6fb04fa8d122fb66ea7a
이찬진 <chanjin23@naver.com>
2025-02-06 21:16:33 +0900

    백준1303 전쟁-전투 : bfs 이용, 걸린시간 : 20min

--- src/baekjoon/codingtest_loadmap/dfs_bfs/Sol1303.java
+++ src/baekjoon/codingtest_loadmap/dfs_bfs/Sol1303.java
@@ -1,67 +1,78 @@
 package baekjoon.codingtest_loadmap.dfs_bfs;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Sol1303 {
     //전투
-    static int n,m, count = 0;
+    static int n, m, totalB, totalW;
     static char[][] arr;
-    static boolean[][] check;
-    static int[] dx,dy;
-    public static void main(String[] args) throws IOException {
-        int enemyTotal=0;
-        int ourTotal=0;
-        dx = new int[]{-1, 0, 1, 0};
-        dy = new int[]{0, 1, 0, -1};
+    static int[][] check;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());   //가로
-        m = Integer.parseInt(st.nextToken());   //세로
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
         arr = new char[m][n];
-        check = new boolean[m][n];
+        check = new int[m][n];
+        totalB = 0;
+        totalW = 0;
 
         for (int i = 0; i < m; ++i) {
-            char[] ch = br.readLine().toCharArray();
-            for (int j = 0; j < n; ++j) {
-                arr[i][j] = ch[j];
+            char[] input = br.readLine().toCharArray();
+            for (int j = 0; j < input.length; ++j) {
+                arr[i][j] = input[j];
             }
         }
 
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                count=0;
-                if (arr[i][j] == 'W' && !check[i][j]) {
-                    check[i][j]=true;
-                    count++;
-                    dfs(i, j, 'W');
-                    ourTotal+=(count*count);
-                }
-                if(arr[i][j] == 'B' && !check[i][j]){
-                    check[i][j]=true;
-                    count++;
-                    dfs(i, j, 'B');
-                    enemyTotal += (count * count);
+                if (check[i][j] == 0) {
+                    check[i][j] = 1;
+                    bfs(i, j, arr[i][j]);
                 }
             }
         }
-        System.out.println(ourTotal+" "+enemyTotal);
 
+        System.out.println(totalW + " " + totalB);
     }
 
-    public static void dfs(int x, int y, char color) {
-        for (int i = 0; i < 4; ++i) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n && arr[nx][ny] == color && !check[nx][ny]) {
-                check[nx][ny]=true;
-                count++;
-                dfs(nx, ny, color);
+    public static void bfs(int x, int y, char army) {
+        Queue<Point> q = new LinkedList<>();
+        q.offer(new Point(x, y));
+        int count = 1;
+        while (!q.isEmpty()) {
+            Point tmp = q.poll();
+            for (int i = 0; i < 4; ++i) {
+                int nx = tmp.x + dx[i];
+                int ny = tmp.y + dy[i];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && check[nx][ny] == 0 &&arr[nx][ny]==army) {
+                    check[nx][ny] = 1;
+                    q.offer(new Point(nx, ny));
+                    count++;
+                }
             }
+        }
+        if (army == 'W') {
+            totalW += (count * count);
+        } else {
+            totalB += (count * count);
+        }
+    }
+
+    public static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
