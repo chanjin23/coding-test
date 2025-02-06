feb86096fda8ec0173c8e43c6ed0ef3de280c790
이찬진 <chanjin23@naver.com>
2025-02-06 21:49:17 +0900

    백준2178 미로 탐색 : bfs 이용, 걸린시간 : 28min

--- src/baekjoon/codingtest_loadmap/dfs_bfs/Sol2178.java
+++ src/baekjoon/codingtest_loadmap/dfs_bfs/Sol2178.java
@@ -1,59 +1,58 @@
 package baekjoon.codingtest_loadmap.dfs_bfs;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Sol2178 {
     //미로 탐색
-    public static void main(String[] args) throws IOException {
+    static int n, m;
+    static int[][] arr, answer;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[n][m];
-        int[][] answer = new int[n][m];
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, 1, 0, -1};
+        arr = new int[n][m];
+        answer = new int[n][m];
 
         for (int i = 0; i < n; ++i) {
-            char[] ch = br.readLine().toCharArray();
+            String[] input = br.readLine().split("");
             for (int j = 0; j < m; ++j) {
-                arr[i][j] = ch[j]-'0';
+                arr[i][j] = Integer.parseInt(input[j]);
             }
         }
 
-
-        Queue<Point> q = new LinkedList<>();
         answer[0][0] = 1;
         arr[0][0] = 0;
-        q.offer(new Point(0, 0));
-
-        while (!q.isEmpty()) {
-            int len = q.size();
-            for (int i = 0; i < len; ++i) {
-                Point p = q.poll();
-                for (int j = 0; j < 4; ++j) {
-                    int nx = p.x + dx[j];
-                    int ny = p.y + dy[j];
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < m && arr[nx][ny] == 1) {
-                        arr[nx][ny] = 0;
-                        q.offer(new Point(nx, ny));
-                        answer[nx][ny] = answer[p.x][p.y] + 1;
-                    }
-                }
-            }
-        }
+        bfs(0, 0);
 
         System.out.println(answer[n - 1][m - 1]);
     }
 
-    private static class Point {
+    public static void bfs(int x, int y) {
+        Queue<Point> q = new LinkedList<>();
+        q.offer(new Point(x, y));
+        while (!q.isEmpty()) {
+            Point tmp = q.poll();
+            if (tmp.x == n - 1 && tmp.y == m - 1) break;
+            for (int i = 0; i < 4; ++i) {
+                int nx = tmp.x + dx[i];
+                int ny = tmp.y + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && arr[nx][ny] == 1) {
+                    arr[nx][ny] = 0;
+                    answer[nx][ny] = answer[tmp.x][tmp.y] + 1;
+                    q.offer(new Point(nx, ny));
+                }
+            }
+        }
+    }
+
+    public static class Point {
         int x;
         int y;
 
