88f9fab16c6e9542c81bd678cfa0ee381b068b65
이찬진 <chanjin23@naver.com>
2025-02-07 20:22:14 +0900

    백준16953 A->B : dfs 이용, 시간계산: 약 0.1초 걸린시간 : 10min

--- src/baekjoon/codingtest_loadmap/dfs_bfs/Sol16953.java
+++ src/baekjoon/codingtest_loadmap/dfs_bfs/Sol16953.java
@@ -4,28 +4,28 @@ import java.util.Scanner;
 
 public class Sol16953 {
     //A->B
-    static long a, b,min;
+    static long a, b, answer;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        a = sc.nextInt();
-        b = sc.nextInt();
 
-        min = Integer.MAX_VALUE;
+        a = sc.nextLong();
+        b = sc.nextLong();
+        answer = Integer.MAX_VALUE;
 
         dfs(a, 1);
 
-        if(min==Integer.MAX_VALUE) System.out.println(-1);
-        else System.out.println(min);
+        if (answer == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(answer);
     }
 
-    private static void dfs(long num, int count) {
-        if (num > b) return;
-        if (num == b) {
-            min = Math.min(min, count);
+    public static void dfs(long value, int count) {
+        if (value > b) return;
+        if (value == b) {
+            answer = Math.min(answer, count);
+            return;
         }
-        dfs(num * 2, count + 1);
-        dfs(num * 10 + 1, count + 1);
+        dfs(value * 2, count + 1);
+        dfs(value * 10 + 1, count + 1);
     }
-
 }
