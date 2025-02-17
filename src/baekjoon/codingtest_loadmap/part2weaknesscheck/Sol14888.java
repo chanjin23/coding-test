c40ede7950411581967ab38935dae77fd57615e9
이찬진 <chanjin23@naver.com>
2025-02-17 17:00:59 +0900

    백준14888 : dfs 이용

--- src/baekjoon/codingtest_loadmap/part2weaknesscheck/Sol14888.java
+++ src/baekjoon/codingtest_loadmap/part2weaknesscheck/Sol14888.java
@@ -1,28 +1,33 @@
 package baekjoon.codingtest_loadmap.part2weaknesscheck;
 
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Sol14888 {
     //연산자 끼워넣기
-    static int n,max, min;
-    static int[] arr;
-    static int[] operator;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+    static int n, max, min;
+    static int[] arr, mid;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
         max = Integer.MIN_VALUE;
         min = Integer.MAX_VALUE;
         arr = new int[n];
+        mid = new int[4];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; ++i) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        operator = new int[4];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; ++i) {
-            operator[i] = sc.nextInt();
+            mid[i] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(1,arr[0]);
+        dfs(1, arr[0]);
+
         System.out.println(max);
         System.out.println(min);
     }
@@ -31,26 +36,22 @@ public class Sol14888 {
         if (level == n) {
             max = Math.max(max, total);
             min = Math.min(min, total);
-        } else {
-            for (int i = 0; i < 4; ++i) {
-                if (operator[i] > 0) {
-                    operator[i]--;
-                    if (i == 0) {
-                        //덧셈
-                        dfs(level + 1, total + arr[level]);
-                    } else if (i == 1) {
-                        //뺼샘
-                        dfs(level + 1, total - arr[level]);
-                    } else if (i == 2) {
-                        //곱셈
-                        dfs(level + 1, total * arr[level]);
-                    } else if (i == 3) {
-                        //나눗셈
-                        dfs(level + 1, total / arr[level]);
-                    }
-                    operator[i]++;
-                }
+            return;
+        }
+
+        for (int i = 0; i < 4; ++i) {
+            if (mid[i] <= 0) continue;
+            mid[i]--;
+            if (i == 0) {
+                dfs(level + 1, total + arr[level]);
+            } else if (i == 1) {
+                dfs(level + 1, total - arr[level]);
+            } else if (i == 2) {
+                dfs(level + 1, total * arr[level]);
+            } else {
+                dfs(level + 1, total / arr[level]);
             }
+            mid[i]++;
         }
     }
 }
