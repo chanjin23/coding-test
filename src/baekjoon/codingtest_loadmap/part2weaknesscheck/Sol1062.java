87990074056a6d9c91e20c115271b3cff9641e3d
이찬진 <chanjin23@naver.com>
2025-02-17 23:30:13 +0900

    백준1062 : dfs

--- src/baekjoon/codingtest_loadmap/part2weaknesscheck/Sol1062.java
+++ src/baekjoon/codingtest_loadmap/part2weaknesscheck/Sol1062.java
@@ -1,67 +1,61 @@
 package baekjoon.codingtest_loadmap.part2weaknesscheck;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Sol1062 {
     //가르침
-    static String[] input;
+    static int n, k, answer;
     static boolean[] alphabet;
-    static int k, answer = 0, n;
+    static String[] arr;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-
-        input = new String[n];
         alphabet = new boolean[26];
+        arr = new String[n];
+        answer = 0;
+
         alphabet['a' - 'a'] = true;
-        alphabet['c' - 'a'] = true;
-        alphabet['i' - 'a'] = true;
         alphabet['n' - 'a'] = true;
         alphabet['t' - 'a'] = true;
+        alphabet['i' - 'a'] = true;
+        alphabet['c' - 'a'] = true;
+
         for (int i = 0; i < n; ++i) {
-            input[i] = br.readLine();
+            arr[i] = br.readLine();
         }
 
-        if (k < 5) {
-            System.out.println(0);
-            return;
-        } else if (k == 26) {
-            System.out.println(n);
-            return;
-        }
+        dfs(k - 5, 0);
 
-        dfs(0,0);
         System.out.println(answer);
     }
 
-    public static void dfs(int alpha, int level) {
-        if (level == k - 5) {
+    public static void dfs(int count, int pos) {
+        if (count == 0) {
             int total = 0;
             for (int i = 0; i < n; ++i) {
-                boolean flag = false;
-                for (char c : input[i].toCharArray()) {
+                boolean flag = true;
+                for (char c : arr[i].toCharArray()) {
                     if (!alphabet[c - 'a']) {
-                        flag = true;
+                        flag = false;
                         break;
                     }
                 }
-                if (!flag) total++;
+                if (flag) total++;
             }
             answer = Math.max(total, answer);
-        } else {
-            for (int i = alpha; i < 26; ++i) {
-                if (!alphabet[i]) {
-                    alphabet[i] = true;
-                    dfs(i,level + 1);
-                    alphabet[i] = false;
-                }
+            return;
+        }
+
+        for (int i = pos; i < 26; ++i) {
+            if (!alphabet[i]) {
+                alphabet[i] = true;
+                dfs(count - 1, i);
+                alphabet[i] = false;
             }
         }
     }
