4f819a37233cde08adc4661cd3f3e5e58bb6356b
이찬진 <chanjin23@naver.com>
2025-02-17 16:45:20 +0900

    백준2460 : 구현문제

--- src/baekjoon/codingtest_loadmap/part1basic/Sol2460.java
+++ src/baekjoon/codingtest_loadmap/part1basic/Sol2460.java
@@ -6,19 +6,16 @@ public class Sol2460 {
     //지능형 기차2
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int max=Integer.MIN_VALUE;
-        int count=0;
+        int max = Integer.MIN_VALUE;
+        int total = 0;
 
         for (int i = 0; i < 10; ++i) {
             int out = sc.nextInt();
             int in = sc.nextInt();
-
-            count = count + in - out;
-            if(count>10000) count=10000;
-
-            max = Math.max(max, count);
+            total = total - out + in;
+            max = Math.max(max, total);
         }
+
         System.out.println(max);
     }
 }
